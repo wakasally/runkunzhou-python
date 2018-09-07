eac8b007c278648b337cec41bcfbf33dc07db526
19RiversKele <keletamasrivers@gmail.com>
2018-09-07 09:17:14 -0700

    Unit1Lab

--- HelloTacoma/Hello Tacoma1.py
+++ HelloTacoma/Hello Tacoma1.py
@@ -1,3 +1,7 @@
 print ("Hello Tacoma")#this is my first project
+mystring=input()
+print(2+2)*4
+x=4
+y=6
 
 
---
+++ Unit1Lab/Unit1Lab.py
@@ -0,0 +1,13 @@
+print ("I love Python")
+print ("What is your Name")
+myName = input()
+print (myName)
+#print ("My name is ")+myName
+
+answer1 = input()
+print(answer1)
+answer2 = input()
+print(answer1+answer2)
+print(int(answer1)+int(answer2))
+
+
