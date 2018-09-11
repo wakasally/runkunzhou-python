1e3b108a37170983076caa9b7faa495740df924b
19RiversKele <keletamasrivers@gmail.com>
2018-09-11 09:18:00 -0700

    Unit2BLab

---
+++ Unit2BLab/Unit2BLab.py
@@ -0,0 +1,13 @@
+x=int(input("enter a number"))
+if (x>=90):
+    print("Yahoo you got an A!")
+elif (x>=80):
+    print("Good you got a B!")
+elif (x>=70):
+    print("Okay you got a C!")
+
+print("what is the color")
+color=input()
+if color=blue:
+    print("The sky is blue")
+
---
+++ Unit2Lab/Unit2ALab.py
@@ -0,0 +1,11 @@
+print(5+2.1)
+print(2.4+4)
+print(int(4.3+2.1))
+i=100
+j=200
+x=300
+y=100
+print (i==j)or(i>x)
+print (i<j)and(i>=y)
+print (i!=j)and(i!=x)
+print (i<j)and(i<x)and(i<y)
